f7dbf86a2a7ecf93a47f03f9c4d8b01ac9a0cf43
Gabriel Aguiar <gabriel.aguiar@sistemafiep.org.br>
2020-09-02 15:39:44 -0300

    manual insert

--- Exercício/gabriel-jonas/db.py
+++ Exercício/gabriel-jonas/db.py
@@ -52,13 +52,56 @@ def generateRandomData(db):
         
 
 
+def manualInsert(db):
+    x = int(input ("Deseja adicionar 1-Pessoa, 2-Site, 3-Visita, 4-Survey? "))
+    cur = db.cursor()
+
+    if (x==1):
+        id_person = input("id da pessoa: ")
+        name = input("nome: ")
+        family_name = input("sobrenome: ")
+        queryInsertPerson = "INSERT INTO Person VALUES('{}','{}','{}')".format(id_person, name,family_name)
+        cur.execute(queryInsertPerson)
+        print ("{}, {}, {}".format(id_person, name,family_name))
+    elif (x==2):
+        site_name = input("nome do site: ")
+        lat = float(input("lat: "))
+        lon = float(input("lon: "))
+        print ("{}, {}, {}".format(site_name, lat, lon))
+        queryInsertSite = "INSERT INTO Site VALUES('{}',{},{})".format(site_name, lat, lon)
+        cur.execute(queryInsertSite)
+    elif (x==3):
+        taken = int(input("id da leitura: "))
+        site_name = input("nome do site: ")
+        date = input("data (YYYY-MM-DD): ")
+        print ("{}, {}, {}".format(taken, site_name, date))
+        quertInsertVisit = "INSERT INTO Visited(id, site, dated) VALUES({},'{}','{}')".format(taken+i, site_name, date)
+        cur.execute(quertInsertVisit)
+    elif (x==4):
+        taken = int(input("id da leitura: "))
+        id_person = input("id da pessoa: ")
+        quant = input("quant: ")
+        leitura = float(input("leitura: "))
+
+        print ("{}, {}, {}, {}".format(taken, id_person, quant, leitura ))
+        quertInsertSurvey = "INSERT INTO Survey VALUES({},'{}','{}',{})".format(taken, id_person, quant, leitura )
+        cur.execute(quertInsertSurvey)
+
+    else:
+        print("Opção não encontrada")
+    
+    db.commit()
 
 
 db = MySQLdb.connect(host="127.0.0.1",    
                      user="gabriel",         
                      passwd="highpass2",  
                      db="dbClass")       
-generateRandomData(db)
+#generateRandomData(db)
+manualInsert(db)
+
+
+
 '''cur = db.cursor()
 
 #cur.execute("INSERT INTO Person VALUES('dyer2','William222','Dyer222');")
